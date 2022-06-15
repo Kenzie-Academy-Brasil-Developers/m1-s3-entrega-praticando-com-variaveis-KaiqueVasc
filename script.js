2882eca01d2feaa52b67f2ae5479c1923709b7e4
Kaique Amorim <106697668+KaiqueVasc@users.noreply.github.com>
2022-06-15 15:22:36 -0300

    First Version

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome ="Kaique";
+let sobrenome ="Amorim";
+let anoNascimento = 1998;
+let cidade ="Santos";
+let anoAtual = 2022
+
+let anos= Number(anoAtual) - Number(anoNascimento)
+
+console.log (`Eu sou o ${nome+sobrenome} tenho ${anos} e moro em ${cidade}`)
+
