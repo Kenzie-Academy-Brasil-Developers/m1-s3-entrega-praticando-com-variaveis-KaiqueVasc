dfd69ff25ccfcbe9de83267e8cf0160ee3218076
Kaique Amorim <106697668+KaiqueVasc@users.noreply.github.com>
2022-06-15 15:29:00 -0300

    Final Version

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ let anoAtual = 2022
 
 let anos= Number(anoAtual) - Number(anoNascimento)
 
-console.log (`Eu sou o ${nome+sobrenome} tenho ${anos} e moro em ${cidade}`)
+console.log (`Eu sou o ${nome+" "+sobrenome} tenho ${anos} e moro em ${cidade}`)
 
